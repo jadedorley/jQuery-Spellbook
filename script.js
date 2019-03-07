e0eab3ed751888d4d48f4ee8a7685eeadfeaa0ef
jadedorley <43240409+jadedorley@users.noreply.github.com>
2019-03-07 08:30:21 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#abc").click(function() {
+    $(".img1").slideUp();
+
+});
+$("#ab").mouseover(function() {
+    $(".img2").fadeOut();
+
+});
+$("#abcd").click(function() {
+    $(".img3").slideToggle();
+
+});
